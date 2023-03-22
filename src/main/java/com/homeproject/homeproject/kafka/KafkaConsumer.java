8a600fa1e91390d588faac3e3d03ddc629a9053d
anuragkumar <anuragkr4713@gmail.com>
2023-03-23 00:58:07 +0530

    added-kafka consumer

---
+++ src/main/java/com/homeproject/homeproject/kafka/KafkaConsumer.java
@@ -0,0 +1,16 @@
+package com.homeproject.homeproject.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KafkaConsumer {
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group")
+    public void consume(String message){
+        LOG.info("Consuming from my_topic with message {}",message);
+    }
+}

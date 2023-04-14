c6ca0b17fd28a6f1e94a73d7c15b978468fda3b9
anuragkumar <anuragkr4713@gmail.com>
2023-04-14 23:47:28 +0530

    removed useless constructos

--- src/main/java/com/homeproject/homeproject/test/Cat.java
+++ src/main/java/com/homeproject/homeproject/test/Cat.java
@@ -1,9 +1,6 @@
 package com.homeproject.homeproject.test;
 
 public class Cat implements Animal {
-    public void Cat() {
-    }
-
     public void sound() {
         System.out.println("Meow!");
     }
--- src/main/java/com/homeproject/homeproject/test/Dog.java
+++ src/main/java/com/homeproject/homeproject/test/Dog.java
@@ -1,9 +1,6 @@
 package com.homeproject.homeproject.test;
 
 public class Dog implements Animal {
-    public void Dog() {
-    }
-
     public void sound() {
         System.out.println("Woof!");
     }

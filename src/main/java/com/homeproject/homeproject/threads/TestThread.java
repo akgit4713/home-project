914f5222410442c290cff0c00148c103c0252be5
anuragkumar <anuragkr4713@gmail.com>
2023-05-01 17:32:06 +0530

    added synchronisation example

---
+++ src/main/java/com/homeproject/homeproject/threads/TestThread.java
@@ -0,0 +1,29 @@
+package com.homeproject.homeproject.threads;
+
+public class TestThread {
+    private static final int MAX_NUM = 100;
+    private static int currentNum = 1;
+
+    public static void main(String[] args) {
+        Object lock = new Object();
+
+        for (int i = 0; i < MAX_NUM; i++) {
+            final int threadNum = i + 1;
+            new Thread(() -> {
+                synchronized (lock){
+                    while (currentNum != threadNum) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    System.out.println(threadNum+" "+Thread.currentThread().getName());
+                    currentNum++;
+                    lock.notifyAll();
+                }
+            }).start();
+        }
+    }
+}

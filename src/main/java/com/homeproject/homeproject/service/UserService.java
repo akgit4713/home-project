c7361c2a73dad0a1a613c7b642dd54ea8fcbebef
anuragkumar <anuragkr4713@gmail.com>
2023-05-17 22:12:16 +0530

    autowired

--- src/main/java/com/homeproject/homeproject/service/UserService.java
+++ src/main/java/com/homeproject/homeproject/service/UserService.java
@@ -4,6 +4,7 @@ import com.homeproject.homeproject.dao.UserRepository;
 import com.homeproject.homeproject.entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
@@ -16,6 +17,7 @@ public class UserService {
 
     private final UserRepository userRepository;
 
+    @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
